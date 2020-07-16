62a6d35384d9ceec73f59226ed41a9fe5489b166
Sławomir Paszko <folkus@gmail.com>
2020-07-16 10:21:15 +0200

    Added better python3 version.

--- pyEdfToCsv.py
+++ pyEdfToCsv.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python2
-from edf2csv import EDF
+#!/usr/bin/python3
+import pyedflib
+import numpy as np
 import argparse
 
 # Arguments and config
@@ -9,11 +10,21 @@ parser.add_argument('-i', '--input', type=str,
                     required=True, help='Input EDF file')
 args = parser.parse_args()
 
-edf = EDF(args.input)
 
-signal = edf.signal()
-info = edf.info()
+# Open EDF file
+f = pyedflib.EdfReader(args.input)
+print('Opened', args.input)
+n = f.signals_in_file
+print('%u signals in file.' % (n))
+signal_labels = f.getSignalLabels()
+print('Signal labels in file : ', signal_labels)
 
+signals = []
+for i in np.arange(n):
+    print('Reading signal %u, samples %u' % (i, f.getNSamples()[i]))
+    signal = f.readSignal(i)
+    signals.append(signal)
+
+# Create .csv
 print('Creation of .csv.')
-edf.ann_to_csv()
-edf.signal_to_csv()
+print(sigbufs)
